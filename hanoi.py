a2640612440664637c1794465bf7d5b2174e9a98
mpalevitz <mpalevitz@gmail.com>
2019-04-11 22:50:21 -0400

    v.Final Towers of Hanoi

---
+++ hanoi.py
@@ -0,0 +1,66 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+
+
+#Set up the Game
+num_disks = int(input("\nHow many disks do you want to play with?\n"))
+
+while num_disks < 3:
+  num_disks = int(input("Enter a number great than or equal to 3\n"))
+  
+for i in range(num_disks, 0, -1):
+  left_stack.push(i)
+  
+num_optimal_moves = 2**num_disks - 1
+
+print("\nThe fastest you can solve this game is in {nom} moves".format(nom=num_optimal_moves))
+#Get User Input
+def get_input():
+  choices = [stack.name[0] for stack in stacks]
+  while True:
+    for i in range(len(stacks)):
+      name = stacks[i].name
+      letter = choices[i]
+      print("Enter {ltr} for {name}".format(ltr=letter, name=name))
+    user_input = input("")
+    
+    if user_input in choices:
+      for i in range(len(stacks)):
+        if user_input == choices[i]:
+          return stacks[i]
+        
+#Play the Game
+num_user_moves = 0
+
+while right_stack.size != num_disks:
+  print("\n\n\n...Current Stacks...")
+  for stack in stacks:
+    stack.print_items()
+  while True:
+    print("\nWhile stack do you want to move from?\n")
+    from_stack = get_input()
+    print("\nWhich stack do you want to move to?\n")
+    to_stack = get_input()
+    
+    if from_stack.is_empty():
+      print("\n\nInvalid Move. Try Again")
+    elif to_stack.is_empty() or from_stack.peek() < to_stack.peek():
+      disk = from_stack.pop()
+      to_stack.push(disk)
+      num_user_moves += 1
+      break
+    else:
+      print("\n\nInvalid Move. Try Again")
+      
+print("\n\nYou completed the game in {moves} moves, and the optimal number of moves is {opt}".format(moves=num_user_moves, opt=num_optimal_moves))
